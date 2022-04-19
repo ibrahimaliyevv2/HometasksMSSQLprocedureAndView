58c615151ec48e6b5388d6be58baf81f2f50de28
Ibrahim Aliyev <100702986+ibrahimaliyevv2@users.noreply.github.com>
2022-04-19 15:08:37 +0400

    Update LibraryDB.sql

--- LibraryDB.sql
+++ LibraryDB.sql
@@ -12,17 +12,10 @@ CREATE TABLE Authors
 CREATE TABLE Books
 (
     Id INT PRIMARY KEY IDENTITY,
-    Name NVARCHAR(200),
-    PageCount INT,
+    Name NVARCHAR(200) CHECK(LEN(Name) BETWEEN 2 AND 100),
+    PageCount INT CHECK (PageCount >2 ),
     AuthorId INT FOREIGN KEY REFERENCES Authors(Id)
 )
-
-ALTER TABLE Books
-ADD CONSTRAINT CK_Len CHECK(LEN(Name) BETWEEN 2 AND 100)
-
-
-ALTER TABLE Books
-ADD CONSTRAINT CK_Min CHECK (PageCount >2 )
 
 
 INSERT INTO Authors
@@ -41,73 +34,52 @@ SELECT * FROM Authors
 
 SELECT * FROM Books
 
-CREATE VIEW usp_showbook
+
+--1
+CREATE VIEW showbooks
 AS
-SELECT BK.Id,BK.Name ,BK.PageCount,AU.Name+' '+AU.Surname AS 'FullName' FROM Books AS BK
-JOIN Authors AS AU ON BK.AuthorId = AU.Id
+SELECT Books.Id, Books.Name, Books.PageCount, Authors.Name + ' ' + Authors.Surname AS 'AuthorFullName' FROM Books
+JOIN Authors ON Books.AuthorId = Authors.Id
 
-ALTER VIEW usp_showbook
+
+--2
+CREATE PROCEDURE usp_showbooks
+@searched NVARCHAR(20)
 AS
-SELECT BK.Id,BK.Name AS 'BookName' ,BK.PageCount,AU.Name+' '+AU.Surname AS 'FullName' FROM Books AS BK
-JOIN Authors AS AU ON BK.AuthorId = AU.Id
-
-SELECT * FROM usp_showbook
-
+SELECT Books.Id, Books.Name, Books.PageCount, Authors.Name + ' ' + Authors.Surname AS 'AuthorFullName' FROM Books
+JOIN Authors ON Books.AuthorId = Authors.Id
+WHERE Books.Name LIKE @searched OR  Authors.Name + ' ' + Authors.Surname LIKE @searched
 
 
-CREATE PROCEDURE usp_filterdBooks
-@value VARCHAR(25)
-AS
-SELECT * FROM usp_showbook AS SHBK
-WHERE SHBK.FullName LIKE (@value) OR SHBK.BookName LIKE (@value)
-
-EXEC usp_filterdBooks 'Semed Vurgun'
-
-CREATE PROCEDURE usp_deleteauthors
-@value INT
-AS 
-DELETE FROM Authors
-WHERE Id = @value
-
-EXEC usp_deleteauthors 7
-
-
-CREATE PROCEDURE usp_insertauthors
-@valuename VARCHAR(25),
-@valuesurname VARCHAR(30)
+--3
+CREATE PROCEDURE usp_insert
+@name NVARCHAR(20), @surname NVARCHAR(20)
 AS
 INSERT INTO Authors
 VALUES
-(@valuename,@valuesurname)
+(@name, @surname)
 
-EXEC usp_insertauthors 'Xelil','Rza'
 
-CREATE PROCEDURE usp_updateauthors
-@value INT,
-@valuename VARCHAR(25)
+--4
+CREATE PROCEDURE usp_update
+@Id INT, @name NVARCHAR(20), @surname NVARCHAR(20)
 AS
 UPDATE Authors
-SET Authors.Name=@valuename
-where Id = @value
-
-EXEC usp_updateauthors 8 ,'Cabbarov'
+SET Name = @name, Surname = @surname
+WHERE Id = @id
 
 
-SELECT BK.MAX(PageCount) FROM Authors AS AU
-JOIN Books AS BK ON AU.Id = BK.AuthorId
-GROUP BY(PageCount)
+--5
+CREATE PROCEDURE usp_delete
+@id INT
+AS
+DELETE FROM Authors
+WHERE Id = @id
 
 
-
-SELECT MAX(PageCount) AS 'MAXPAGE',AuthorId from Books GROUP BY AuthorId 
-
-
-
-CREATE VIEW usp_showinfo
-AS 
-SELECT AU.Id,AU.Name+' '+AU.Surname AS 'FullName',MAXPAGE,(SELECT COUNT(Id)FROM Books WHERE Books.AuthorId=AU.Id)AS'AuthorBookCount' FROM Authors AS AU
-JOIN 
-(SELECT MAX(PageCount) AS 'MAXPAGE',AuthorId from Books GROUP BY AuthorId )AS PR ON
-PR.AuthorId= AU.Id
-
-SELECT * FROM usp_showinfo
+--6
+CREATE VIEW showauthorinfo
+AS
+SELECT Authors.Id, Authors.Name + ' ' + Authors.Surname AS 'FullName', 
+(SELECT COUNT(Id) FROM Books) AS 'BooksCount', 
+(SELECT MAX(PageCount) FROM Books) AS 'MaxPageCount' FROM Authors
